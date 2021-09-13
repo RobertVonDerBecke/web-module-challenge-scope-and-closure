1207bdecfff0900cd590d36022571351c8a6cdba
MRobertPHD <77177178+MRobertPHD@users.noreply.github.com>
2021-09-13 13:35:41 -0500

    All task required completed

--- index.js
+++ index.js
@@ -28,11 +28,13 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  Counter one return a function, counter two increments a counter
   2. Which of the two uses a closure? How can you tell?
-  
+  counter 1 uses a closure. You can tell because the variable 'count' is incremented in the counter function
+  but declared in the counterMaker func.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better? 
+     counter one code would be better if i was tracking multiple counter values. 
 */
 
 // counter1 code
@@ -62,10 +64,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.round(Math.random()*2);
 }
-
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -81,17 +82,29 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(cdFunc, numInnPlayed){
+  let home = 0;
+  let away = 0;
+  for(let i = 0;i<numInnPlayed;i++){
+    home += cdFunc();
+    away += cdFunc();
+  }
+  return {
+    Home: home,
+    Away: away
+  }
 }
-
+console.log(finalScore(inning, 9))
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCDFunc) {
+  return {
+    Home: inningCDFunc(),
+    Away: inningCDFunc()
+  }
 }
 
 
